2bebdce9b19bebead04ef46729cbb99b4d1e8643
xiaoyaosheny <xiaoyaosheny@qq.com>
2026-07-09 15:02:24 +0800

    change to LazyNvim
    Use LazyNvim instead of AstroNvim. The configs of AstroNvim are in
    'astronvim' branch.

---
+++ lua/plugins/sidekick.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/sidekick.nvim",
+    opts = {
+      nes = {
+        enabled = false,
+      },
+      copilot = {
+        status = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
